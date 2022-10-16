18b372c1947bcd0ae8551cf208d9dcc914e8cae3
fadimichael <fadi.michael@gmx.de>
2022-10-17 01:01:23 +0200

    responsieve verbessert

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,9 @@ const darkModeToggle = document.querySelector("#dark-mode-toggle");
 const darkModeName = document.querySelector("#dark-mode-name");
 const wrapper = document.querySelector(".wrapper");
 const aTags = document.querySelectorAll(".wrap");
-console.log(aTags);
+
+darkModeToggle.innerHTML = "&#xf205";
+darkModeName.innerHTML = "Dark Mode";
 
 darkModeToggle.addEventListener("click", function () {
   this.classList.toggle("active");
@@ -11,8 +13,10 @@ darkModeToggle.addEventListener("click", function () {
     singleAtag.classList.toggle("active");
     if (darkModeToggle.classList.contains("active")) {
       darkModeName.innerHTML = "Light Mode";
+      darkModeToggle.innerHTML = "&#xf204";
     } else {
       darkModeName.innerHTML = "Dark Mode";
+      darkModeToggle.innerHTML = "&#xf205";
     }
   });
 });
